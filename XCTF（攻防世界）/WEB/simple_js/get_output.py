57d2c2a5b193ba4a546392c4fc6d041cd94c626a
ThoseBygones <1273789365@qq.com>
2020-06-20 23:35:03 +0800

    新增WP：simple_js

---
+++ XCTF（攻防世界）/WEB/simple_js/get_output.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 20 22:33:53 2020
+
+@author: ThoseBygones
+"""
+
+
+msg = [70,65,85,88,32,80,65,83,83,87,79,82,68,32,72,65,72,65]
+
+output = ""
+
+for i in msg:
+    output += chr(i)
+    
+print(output)
